ddbc9b93836c67734b20863f5a16532b168db63e
Justin <masterofleaf@gmail.com>
2022-03-10 16:34:36 +0800

    second commit

--- app/src/main/java/com/tom/chat/MainActivity.kt
+++ app/src/main/java/com/tom/chat/MainActivity.kt
@@ -2,10 +2,40 @@ package com.tom.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import com.tom.chat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        val TAG = MainActivity::class.java.simpleName
+    }
+    lateinit var binding : ActivityMainBinding
+    val personResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){result ->
+        Log.d(TAG, ": ")
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupFunction()
+
     }
+
+    private fun setupFunction(){
+        binding.bHome.setOnClickListener(){
+
+        }
+
+        binding.bSearch.setOnClickListener(){
+
+        }
+
+        binding.bPerson.setOnClickListener(){
+
+        }
+    }
+
 }
